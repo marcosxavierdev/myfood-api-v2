7dc5f053662d9d8763a117f7c1ecce69b2827fbc
Marcos.Xavier <@marcosxavierdev>
2021-11-19 17:11:37 -0300

    primeiro commit

---
+++ src/main/java/br/com/marcosxavierdev/myfood/MyfoodApiV2Application.java
@@ -0,0 +1,13 @@
+package br.com.marcosxavierdev.myfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfoodApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfoodApiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcosxavierdev/myfood/MyfoodApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosxavierdev.myfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfoodApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
