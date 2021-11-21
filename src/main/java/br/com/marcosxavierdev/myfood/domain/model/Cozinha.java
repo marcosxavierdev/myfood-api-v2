f80373d311558f9847dd27e982e195c2b5e39998
Marcos.Xavier <@marcosxavierdev>
2021-11-21 13:46:39 -0300

    criando import.sql

--- src/main/java/br/com/marcosxavierdev/myfood/domain/model/Cozinha.java
+++ src/main/java/br/com/marcosxavierdev/myfood/domain/model/Cozinha.java
@@ -4,17 +4,18 @@ import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tab_cozinha")
 public class Cozinha {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "nome_cozinha")
 	private String nome;
 
 	public Long getId() {
@@ -60,15 +61,15 @@ public class Cozinha {
  * @Entity - define a classe como uma representação de uma entidade (que vai ser
  * traduzida como uma tabela no banco)
  * 
- * @Table - pode dar um nome diferente a tabela no banco de dados (por padrão o
+ * @Table(name = "tab_cozinhas") - pode dar um nome diferente a tabela no banco de dados (por padrão o
  * nome da tabela seria a da classe)
  * 
  * @Id - define que este atributo será o identificador da tabela (chave
  * primária)
  * 
- * @Column - define algumas configurações do atibuto (coluna) da entidade.
+ * @Column(name = "nome_cozinha") - define algumas configurações do atibuto (coluna) da entidade, nesse caso o nome da tabela.
  * 
- * 
+ * @GeneratedValue(strategy = GenerationType.IDENTITY) - configura a responsabilidade do auto-incremento para o banco de dados
  * 
  * 
  */
